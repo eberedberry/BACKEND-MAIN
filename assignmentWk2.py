dd16bf9e92a98eed4cd5dd10613f7a2fbe5c5e50
eberedberry <memepeace90@yahoo.com>
2022-07-02 17:30:36 +0100

    completed assignment for week 2

--- assignmentWk2.py
+++ assignmentWk2.py
@@ -1,1 +1,69 @@
-# NUMBER 1 TO 5
+import math
+
+### NO 1
+
+num1 = int(input("Please input First number :"))
+num2 = int(input("Please input Second number :"))
+num3 = int(input("Please input Third number :"))
+sum = num1 + num2 + num3
+ave = float(sum) /3
+print("The average is {}".format(ave))
+
+
+
+##### NO 2.
+sentence = input("Please input a Sentence :")
+sentence = sentence.split(maxsplit=1)
+rest_sentence = (sentence[1])
+first_word = sentence[0].upper()
+new_sentence = "{} {}".format(first_word, rest_sentence)
+print(new_sentence)
+
+
+
+### NO 3
+txt = "I am learning python"
+x = txt.replace("I", "you")
+print(x)
+
+
+
+###NO 4
+text = "I hope you had fun today in class"
+result = text.count("a")
+print(result)
+
+
+
+### NO 5
+def check_fermat(a, b, c, n):
+    calc = math.pow(a,n) + math.pow(b,n) 
+    result = math.pow(c,n)
+    if (calc == result) and (n > 2):
+        print("Holy smokes, Fermat was wrong!")
+    else:
+        print("No, that doesn’t work")
+
+check_fermat(3,4,5,1)
+check_fermat(3,4,5,2)
+check_fermat(3,4,5,3)
+
+
+
+
+#### NO 6
+a = int(input("please input a :"))
+b = int(input("please input b :"))
+c = int(input("please input c :"))
+n = int(input("please input n :"))
+
+def check_fermat(a, b, c, n):
+    calc = math.pow(a,n) + math.pow(b,n) 
+    result = math.pow(c,n)
+    if (calc == result) and (n > 2):
+        print("Holy smokes, Fermat was wrong!")
+    else:
+        print("No, that doesn’t work")
+
+check_fermat(a,b,c,n)
+
--- first.py
+++ first.py
@@ -1,40 +1,62 @@
-# CREATE A FUNCTION TO TAKE IN CERTAIN VARIABLES 
+import math
 
-def remove_string(new_string , n):
-    print(new_string[0: n])
+# # CREATE A FUNCTION TO TAKE IN CERTAIN VARIABLES 
 
-remove_string("DEVELOPMENT", 7)
-remove_string("SCHOOLBUS", 3)
+# def remove_string(new_string , n):
+#     print(new_string[0: n])
 
-def remove_char(word , n):
-    x = word[n:]
-    print(x)
+# remove_string("DEVELOPMENT", 7)
+# remove_string("SCHOOLBUS", 3)
 
-remove_char("chidiebere", 5)
+# def remove_char(word , n):
+#     x = word[n:]
+#     print(x)
 
-# CONDITIONAL STATEMENTS
-num = 37
+# remove_char("chidiebere", 5)
 
-if num > 32:
-    print(5)
+# # CONDITIONAL STATEMENTS
+# num = 37
 
-elif num > 23:
-    print(10)
+# if num > 32:
+#     print(5)
 
-else:
-    print(4)
+# elif num > 23:
+#     print(10)
 
-if num > 23:
-    print(10)
+# else:
+#     print(4)
 
-# INPUT 
-data = input("Enter your name: ")
-print(type(data))
-print(data.upper())
+# if num > 23:
+#     print(10)
 
-data = int(input("Enter your age:"))
-if data >= 18:
-    print("You can vote")
-else:
-    print("Go back to your house. No be small children ")
+# # INPUT 
+# data = input("Enter your name: ")
+# print(type(data))
+# print(data.upper())
 
+# data = int(input("Enter your age:"))
+# if data >= 18:
+#     print("You can vote")
+# else:
+#     print("Go back to your house. No be small children ")
+
+# print(int(3.222))
+# print(float(3))
+# print(float("33"))
+# print(float("8678"))
+# age = int("23")
+# print(age)
+# print(type(age))
+# address = str(287)
+# print(address)
+# print(type(address))
+
+answer = math.pi *14.0
+# print(int(answer))
+
+# print(math.sin(60))
+# print(10 math.exp(1))
+print(math.ceil(13.465))
+print(math.floor(3.765))
+print(math.floor(13.4065))
+print(math.pow(5,2))
